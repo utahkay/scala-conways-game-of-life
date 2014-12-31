9c72bdff45c4a369eac0ae2c467773e243f240b6
kay-johansen <kay-johansen@pluralsight.com>
2014-12-31 15:43:17 -0700

    Remove unnecessary parentheses

--- src/main/scala/Life.scala
+++ src/main/scala/Life.scala
@@ -1,29 +1,30 @@
 /**
  * Created by kjohansen on 12/31/2014.
  */
+
 case class Life(liveCells: Set[Cell]) {
-  def next(): Life =
+  def next: Life =
     Life(for {
         c <- AllCellsAndNeighbors(liveCells)
-        if (NextGeneration(isLive(c), CountLiveNeighbors(liveCells, c)))
+        if NextGeneration(isLive(c), CountLiveNeighbors(liveCells, c))
       } yield c)
 
   def isLive(c: Cell): Boolean = liveCells contains c
 }
 
 case class Cell(x: Int, y: Int) {
-  def cellAndNeighbors(): Seq[Cell] =
+  def cellAndNeighbors: Seq[Cell] =
     for { xx <- x-1 to x+1; yy <- y-1 to y+1 } yield new Cell(xx, yy)
 }
 
 object CountLiveNeighbors {
   def apply(cells: Set[Cell], origin: Cell): Int =
-    (for { c <- origin.cellAndNeighbors if (c != origin && cells.contains(c)) } yield 1).size
+    (for { c <- origin.cellAndNeighbors if c != origin && cells.contains(c) } yield 1).size
 }
 
 object AllCellsAndNeighbors {
   def apply(cells: Set[Cell]): Set[Cell] =
-    cells.foldLeft(Set[Cell]()) {(s, c) => s ++ c.cellAndNeighbors() }
+    cells.foldLeft(Set[Cell]()) {(s, c) => s ++ c.cellAndNeighbors }
 }
 
 object NextGeneration {
@@ -37,7 +38,7 @@ object NextGeneration {
 class StringRepresentation(val life: Life) {
   val liveCells = life.liveCells
 
-  override def toString(): String = {
+  override def toString: String = {
     def minY: Int = liveCells.minBy(_.y).y
     def maxY: Int = liveCells.maxBy(_.y).y
     (for { y <- minY to maxY } yield rowStr(y)).mkString("\n")
